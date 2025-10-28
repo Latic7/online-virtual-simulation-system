82d59632ac5989bec1d19db34a4525a19ed45161
Latic7 <lhf6714@outlook.com>
2025-10-29 00:44:35 +0800

    Initial commit

---
+++ src/main/java/org/ovss/onlinevirtualsimulationsystem/OnlineVirtualSimulationSystemApplication.java
@@ -0,0 +1,13 @@
+package org.ovss.onlinevirtualsimulationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineVirtualSimulationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineVirtualSimulationSystemApplication.class, args);
+    }
+
+}
